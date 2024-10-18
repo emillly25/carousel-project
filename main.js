71823ed7875d287daf1de2a2995169dcc9ce1219
SEHEE <89740574+emillly25@users.noreply.github.com>
2024-10-18 18:32:21 +0900

    [feat] indicator 기능 추가

--- main.js
+++ main.js
@@ -15,22 +15,25 @@ imageSlideDatas.forEach((slide, index) => {
     // 슬라이드 추가
     const slideElement = document.createElement('li');
     slideElement.classList.add('slide');
-    // 첫 번째 슬라이드는 active 상태로 시작
-    if (index === 0) {
-        slideElement.dataset.active = true;
-    }
 
     // 이미지 추가
     const imgElement = document.createElement('img');
     imgElement.src = slide.src;
     imgElement.alt = slide.alt;
-    slideElement.appendChild(imgElement);
-    slidesContainer.appendChild(slideElement);
 
     // 인디케이터 추가
     const indicator = document.createElement('span');
     indicator.classList.add('indicator');
     indicator.dataset.index = index;
+
+    // 첫 번째 슬라이드는 active 상태로
+    if (index === 0) {
+        slideElement.dataset.active = true;
+        indicator.classList.add('active');
+    }
+
+    slideElement.appendChild(imgElement);
+    slidesContainer.appendChild(slideElement);
     indicatorsContainer.appendChild(indicator);
 });
 
@@ -65,5 +68,37 @@ buttons.forEach((button) => {
 
         //8. 현재 active slide는 active 삭제
         delete activeSlide.dataset.active;
+
+        // 9.인디케이터 업데이트
+        updateIndicators(newSlideIndex);
     });
 });
+
+// 인디케이터 클릭 기능
+indicatorsContainer.addEventListener('click', (e) => {
+    //선택된 인디케이터의 인덱스
+    const selectedIndicatorIndex = parseInt(e.target.dataset.index);
+
+    const slides = document.querySelector('.slide-container');
+    const activeSlide = slides.querySelector('[data-active]');
+
+    // 인디케이터 클릭 시 해당 슬라이드를 active로 변경
+    slides.children[selectedIndicatorIndex].dataset.active = true;
+
+    //기존 activeSlide는 active 삭제
+    delete activeSlide.dataset.active;
+
+    // 인디케이터 업데이트
+    updateIndicators(selectedIndicatorIndex);
+});
+
+function updateIndicators(activeIndex) {
+    const indicators = document.querySelectorAll('.indicator');
+    indicators.forEach((indicator, index) => {
+        if (index === activeIndex) {
+            indicator.classList.add('active');
+        } else {
+            indicator.classList.remove('active');
+        }
+    });
+}
