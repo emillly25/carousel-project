391e489e7b70e598060a882e7efb4b9fabda2dff
SEHEE <89740574+emillly25@users.noreply.github.com>
2024-10-18 18:16:48 +0900

    [fix] 이미지 여러개 추가 가능하도록 동적으로 태그 생성

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+const imageSlideDatas = [
+    { src: 'images/image1.jpg', alt: 'Slide 1' },
+    { src: 'images/image2.jpg', alt: 'Slide 2' },
+    { src: 'images/image3.jpg', alt: 'Slide 3' },
+    { src: 'images/image4.jpg', alt: 'Slide 4' },
+    { src: 'images/image5.jpg', alt: 'Slide 5' },
+    { src: 'images/image6.jpg', alt: 'Slide 6' },
+];
+
+const slidesContainer = document.querySelector('.slide-container');
+const indicatorsContainer = document.querySelector('.indicators');
+
+//이미지 동적으로 추가
+imageSlideDatas.forEach((slide, index) => {
+    // 슬라이드 추가
+    const slideElement = document.createElement('li');
+    slideElement.classList.add('slide');
+    // 첫 번째 슬라이드는 active 상태로 시작
+    if (index === 0) {
+        slideElement.dataset.active = true;
+    }
+
+    // 이미지 추가
+    const imgElement = document.createElement('img');
+    imgElement.src = slide.src;
+    imgElement.alt = slide.alt;
+    slideElement.appendChild(imgElement);
+    slidesContainer.appendChild(slideElement);
+
+    // 인디케이터 추가
+    const indicator = document.createElement('span');
+    indicator.classList.add('indicator');
+    indicator.dataset.index = index;
+    indicatorsContainer.appendChild(indicator);
+});
+
 const buttons = document.querySelectorAll('.carousel-button');
 
 buttons.forEach((button) => {
