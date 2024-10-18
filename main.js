98c5fe1d0fe83676d6585d25e6ef846c2076f50a
SEHEE <89740574+emillly25@users.noreply.github.com>
2024-10-18 18:07:53 +0900

    [feat] 기본 구조 세팅

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
+const buttons = document.querySelectorAll('.carousel-button');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        //1. 이전이냐 다음이냐 결정
+        const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
 
-setupCounter(document.querySelector('#counter'));
+        //2. ul 찾기 = slides
+        const slides = document.querySelector('ul');
+
+        //3. ul안에서 현재 active slide 찾기
+        const activeSlide = slides.querySelector('[data-active]');
+
+        //4. active slide 는 ul안에서 현재 몇번째 인덱스인가
+        const activeSliceIndex = [...slides.children].indexOf(activeSlide);
+
+        let newSlideIndex = activeSliceIndex + offset;
+        //5. 첫번째 -> 마지막
+        if (newSlideIndex < 0) {
+            newSlideIndex = slides.children.length - 1;
+        }
+        //6. 마지막 -> 첫번째
+        if (newSlideIndex >= slides.children.length) {
+            newSlideIndex = 0;
+        }
+
+        //7. 새로운 active slide의 dataset 설정
+        slides.children[newSlideIndex].dataset.active = true;
+
+        //8. 현재 active slide는 active 삭제
+        delete activeSlide.dataset.active;
+    });
+});
